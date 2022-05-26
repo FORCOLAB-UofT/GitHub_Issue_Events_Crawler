c9aa2b0edc990e41c269fe3a2421e12bc90342f3
Waannng <queenie.wang@mail.utoronto.ca>
2022-05-27 03:28:49 +0800

    add convert_to_draft and ready_for_review event types

--- GithubAPI_IssueTimeline_crawler.py
+++ GithubAPI_IssueTimeline_crawler.py
@@ -465,6 +465,42 @@ class repoMethod(scraper.GitHubAPI):
                     'label': '',
                     'body': ''
                 }
+            elif event['event'] == 'convert_to_draft':
+                author = event['actor'] or {}
+                yield {
+                    'event': event['event'],
+                    'author': author.get('login'),
+                    'email': '',
+                    'author_type': author.get('type'),
+                    'author_association': '',
+                    'commit_id': '',
+                    'created_at': event.get('created_at'),
+                    'id': '',
+                    'repo': '',
+                    'type': '',
+                    'state': '',
+                    'assignees': '',
+                    'label':'',
+                    'body': ''
+                }
+            elif event['event'] == 'ready_for_review':
+                author = event['actor'] or {}
+                yield {
+                    'event': event['event'],
+                    'author': author.get('login'),
+                    'email': '',
+                    'author_type': author.get('type'),
+                    'author_association': '',
+                    'commit_id': '',
+                    'created_at': event.get('created_at'),
+                    'id': '',
+                    'repo': '',
+                    'type': '',
+                    'state': '',
+                    'assignees': '',
+                    'label': '',
+                    'body': ''
+                }
             else:
                 yield {
                     'event': event['event'],
