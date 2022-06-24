0ff6c5b878a5083c673e33ee6dee7c9d42885af2
Waannng <90332805+Waannng@users.noreply.github.com>
2022-06-25 04:35:42 +0800

    fix bug

--- repo_list/repo_list.py
+++ repo_list/repo_list.py
@@ -10,6 +10,6 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 mycursor.execute("Insert into new_schema.repo_list (repo_index, repo_name)"
-                 "Select ROW_NUMBER() OVER (ORDER BY morethan500pr.repo_id) + (SELECT MAX(repo_index) FROM repo_list)"
+                 "Select ROW_NUMBER() OVER (ORDER BY morethan500pr.repo_id) + (SELECT count(*) FROM repo_list)"
                  ", REPLACE(repo_url, 'https://api.github.com/repos/', '') From new_schema.morethan500pr")
 mydb.commit()
