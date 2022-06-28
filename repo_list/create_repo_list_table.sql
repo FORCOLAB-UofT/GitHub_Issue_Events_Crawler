33cf5d9d813c6aaf37eb9771634e33ed8eb739b3
Waannng <90332805+Waannng@users.noreply.github.com>
2022-06-29 01:26:57 +0800

    Update create_repo_list_table.sql

--- repo_list/create_repo_list_table.sql
+++ repo_list/create_repo_list_table.sql
@@ -1,4 +1,6 @@
 CREATE TABLE `repo_list` (
-  `repo_index` int NOT NULL,
-  `repo_name` varchar(1000) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `repo_index` int NOT NULL AUTO_INCREMENT,
+  `repo_name` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`repo_index`),
+  UNIQUE KEY `repo_name_UNIQUE` (`repo_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=1262 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
